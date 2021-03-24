a636dbdb00946e39eb7281e199bfe968ff093a61
Alex Forehand <forehand.alex@gmail.com>
2021-03-23 20:18:17 -0700

    Config boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error as StdError;
 
+use cronbot::utils;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn StdError>> {
     Ok(())
---
+++ src/utils/config.rs
@@ -0,0 +1,63 @@
+use serde::{Deserialize, Serialize};
+use std::default::Default;
+use std::fs;
+use std::io;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    token: String,
+
+    #[serde(with = "serde_level_filter")]
+    log_level: log::LevelFilter,
+}
+
+impl Config {
+    pub fn from_file(file_name: &str) -> io::Result<Self> {
+        let conf_string = fs::read_to_string(file_name)?;
+
+        let conf: Config = serde_json::from_str(&conf_string)?;
+
+        Ok(conf)
+    }
+
+    pub fn token(&self) -> &str {
+        &self.token
+    }
+
+    pub fn log_level(&self) -> log::LevelFilter {
+        self.log_level
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            token: "xxxxxxxxxx".to_string(),
+            log_level: log::LevelFilter::Error,
+        }
+    }
+}
+
+// this will allow de/serialization of LevelFilter
+pub mod serde_level_filter {
+    use serde::{Deserializer, Serializer};
+
+    use super::*;
+
+    #[allow(clippy::trivially_copy_pass_by_ref)]
+    pub fn serialize<S>(lf: &log::LevelFilter, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        lf.to_string().serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<log::LevelFilter, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let string = String::deserialize(deserializer)?;
+        let filter = string.parse().map_err(serde::de::Error::custom)?;
+        Ok(filter)
+    }
+}
